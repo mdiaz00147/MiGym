0a721dec8e5dc0cb7cdaa1a1d464217b185d7398
Mario Diaz <mdiaz00147@MacBook-Pro-de-Mario.local>
2016-09-03 18:12:23 -0500

    nueva stats

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -7,12 +7,12 @@ class StatsController < ApplicationController
 	def admin_index
 		@users_active =	User.where(last_login: (Time.now.beginning_of_month)..Time.now.end_of_day)
 		@users_total  =	User.all
-		@lessons_active	=	Lesson.where(updated_at: (Time.now.beginning_of_month)..Time.now.end_of_day)
+		#@lessons_active	=	Lesson.where(updated_at: (Time.now.beginning_of_month)..Time.now.end_of_day)
 
 		beginning_of_day_2	=	''
 
-		@users_new =	User.where(created_at: (Time.now.beginning_of_month)..Time.now.end_of_day)
-		@lessons_morning	=	Lesson.where(updated_at: (Time.now.beginning_of_month)..Time.now.end_of_day)
+		@users_new 			=	User.where(created_at: (Time.now.beginning_of_month)..Time.now.end_of_day)
+		@lessons_morning	=	Lesson.where(start_date: (Time.now.beginning_of_month.beginning_of_day)..Time.now.end_of_day, users_enrolled: (1)..10)
 
 
 	    @num_morning = [] 
