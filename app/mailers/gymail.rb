2e4b938f012b3851ae455981f8563e023603f88d
Mario Diaz <mdiaz00147@MacBook-Pro-de-Mario.local>
2016-09-05 15:08:08 -0500

    correcion

--- app/mailers/gymail.rb
+++ app/mailers/gymail.rb
@@ -2,10 +2,10 @@ class Gymail < ApplicationMailer
 	default from: "elitefitnesscenterco@gmail.com"
    def register_email(user)
     @user = user
-    mail(to: 'sectorstream@hotmail.com', subject: "Bienvenido a Elite Fintess #{user.name.upcase}")
+    mail(to: user.email, subject: "Bienvenido a Elite Fintess #{user.name.upcase}")
    end
    def reminder(user)
     @user = user
-    mail(to: 'sectorstream@hotmail.com', subject: "RECUERDA a Elite Fintess #{user.name.upcase}")
+    mail(to: user.email, subject: "Hace rato no sabemos de ti #{user.name.upcase}")
    end
 end
