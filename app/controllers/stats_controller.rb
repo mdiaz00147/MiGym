702951327873c4358587be3e3a3745ac2a74d1a3
Mario Diaz <mdiaz00147@MacBook-Pro-de-Mario.local>
2016-09-04 21:57:12 -0500

    stats updated

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -6,12 +6,9 @@ class StatsController < ApplicationController
 	end
 	def admin_index
 		@users_active =	User.where(last_login: (Time.now.beginning_of_month)..Time.now.end_of_day)
+		@users_new 	  =	User.where(created_at: (Time.now.beginning_of_month)..Time.now.end_of_day)
 		@users_total  =	User.all
-		#@lessons_active	=	Lesson.where(updated_at: (Time.now.beginning_of_month)..Time.now.end_of_day)
-
-		beginning_of_day_2	=	''
-
-		@users_new 			=	User.where(created_at: (Time.now.beginning_of_month)..Time.now.end_of_day)
+		@courtesis_month = Courtesie.where(created_at: (Time.now.beginning_of_month)..Time.now.end_of_month)
 		@lessons_morning	=	Lesson.where(start_date: (Time.now.beginning_of_month.beginning_of_day)..Time.now.end_of_day, users_enrolled: (1)..10)
 
 
@@ -25,6 +22,5 @@ class StatsController < ApplicationController
 	        end
         end
 
-		@courtesis_month = Courtesie.where(created_at: (Time.now.beginning_of_month)..Time.now.end_of_day)
 	end
 end
