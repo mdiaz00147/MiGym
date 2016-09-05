7b3f2ee05f25f2f0464b72dd8d53d4f3799470ff
Mario Diaz <mdiaz00147@MacBook-Pro-de-Mario.local>
2016-09-05 15:44:25 -0500

    teste

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -39,7 +39,8 @@ class EventsController < ApplicationController
 	end
 	def reminder
 		@user = User.where("last_login < '#{(Time.now) -1.weeks}'")
-		
+		Gymail.reminder(userReminder).deliver_now
+					userReminder.update_attribute(:last_email, DateTime.now)
 		@user.each do |userReminder|
 		if userReminder.last_email.present?
 			if Date.parse(userReminder.last_email.to_s) != Date.parse(Time.now.to_s)
