8e12b72e61c34596854338471150b3b796baa08d
Mario Diaz <mdiaz00147@MacBook-Pro-de-Mario.local>
2016-09-14 16:51:42 -0500

    gfe

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,11 @@ class SessionsController < ApplicationController
   end
   def create
   	 user = User.find_by(email: params[:session][:email].downcase)
-     user.update_attribute(:last_login, DateTime.now)
+     
     if user && user.authenticate(params[:session][:password])
+      user.update_attribute(:last_login, DateTime.now)
       log_in user
-
+      Gymail.register_email(user).deliver_now
       flash[:success] = "Bienvenido de nuevo #{current_user.name.upcase}"
       redirect_to user
     else
