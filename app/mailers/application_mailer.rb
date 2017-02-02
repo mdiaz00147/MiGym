262c3e6b1bac08ddd1e3d9a395ac42289b571c9e
mdiaz00147 <mdiaz00147@gmail.com>
2017-02-02 16:49:32 -0500

    test

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "elitefitnesscenterco@gmail.com"
-  layout 'mailer'
+  # layout 'gymail'
 end
--- app/mailers/gymail.rb
+++ app/mailers/gymail.rb
@@ -1,11 +1,11 @@
 class Gymail < ApplicationMailer
 	default from: "elitefitnesscenterco@gmail.com"
-   def register_email(user)
-    @user = user
-    mail(to: user.email, subject: "Bienvenido a Elite Fintess #{user.name.upcase}")
-   end
-   def reminder(user)
-    @user = user
-    mail(to: user.email, subject: "Hace rato no sabemos de ti #{user.name.upcase}")
-   end
+	def register_email(user)
+		@user = user
+		mail(to: user.email, subject: "Bienvenido a Elite Fintess #{user.name.upcase}")
+	end
+	def reminder(user)
+		@user = user
+		mail(to: user.email, subject: "Hace rato no sabemos de ti #{user.name.upcase}")
+	end
 end
